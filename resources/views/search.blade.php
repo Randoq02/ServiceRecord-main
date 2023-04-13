b7e24c63ff7acf9b628eae6154242cf669d60e27
Mark Catubig <mcatubig27@gmail.com>
2023-04-13 20:29:44 +0800

    update

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <!-- Css plugin -->    
+    <!-- Css plugin -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>Search Employee</title>
 </head>
@@ -21,7 +21,7 @@
         <table class="elementsContainer">
             <tr>
                 <td>
-                    <input type="text" placeholder="Search" class="search">
+                    <input type="text" placeholder="Search Employee" class="search">
                 </td>
                 <td>
                     <i class="fa-solid fa-magnifying-glass"></i>
@@ -31,63 +31,65 @@
     </div>
     <div class="container">
         <table class="table table-striped" style="display:none;">
-        <!-- Table of Contents -->
-        <thead>
-            <h2></h2>
-            <tr class="text-center">
-                <th>Employee NO#</th>
-                <th>FullName</th>
-                <th>Email</th>
-                <th>Position</th>
-                <th>School</th>
-            </tr>
-        </thead>
-        <!-- Table body -->
-        <tbody id="tableBody">
-            <tr class="text-center">
-                <td>0286315</td>
-                <td><a href="{{ route('employee') }}">Mark Anthony H. Catubig</a></td>
-                <td>mcatubig27@gmail.com</td>
-                <td>Office Clerk</td>
-                <td>School Division Office- Marikina</td>
-            </tr>
-            <tr class="text-center">
-                <td>0286315</td>
-                <td><a href="{{ route('employee') }}">John Lester Legaspi</a></td>
-                <td>Legaspi@gmail.com</td>
-                <td>Office Clerk</td>
-                <td>School Division Office- Marikina</td>
-            </tr>
-            <tr class="text-center">
-                <td>0286315</td>
-                <td><a href="{{ route('employee') }}">John Ervin Rola</a></td>
-                <td>rola@gmail.com</td>
-                <td>Office Clerk</td>
-                <td>School Division Office- Marikina</td>
-            </tr>
-            <tr class="text-center">
-                <td>0286315</td>
-                <td><a href="{{ route('employee') }}">Rinrin Caballero</a></td>
-                <td>rinrin@gmail.com</td>
-                <td>Office Clerk</td>
-                <td>School Division Office- Marikina</td>
-            </tr>
-            <tr class="text-center">
-                <td>0286315</td>
-                <td><a href="{{ route('employee') }}">Charles Darwin Janoras</a></td>
-                <td>charles@gmail.com</td>
-                <td>Office Clerk</td>
-                <td>School Division Office- Marikina</td>
-            </tr>
-            <tr class="text-center">
-                <td>0286315</td>
-                <td><a href="{{ route('employee') }}">Randell Ocmer</a></td>
-                <td>ocmer@gmail.com</td>
-                <td>Office Clerk</td>
-                <td>School Division Office- Marikina</td>
-            </tr>
-        </tbody>
-    </div>        
+            <!-- Table of Contents -->
+            <thead>
+                <h2></h2>
+                <tr class="text-center">
+                    <th>Employee NO#</th>
+                    <th>FullName</th>
+                    <th>Email</th>
+                    <th>Position</th>
+                    <th>School</th>
+                </tr>
+            </thead>
+            <!-- Table body -->
+            <tbody id="tableBody">
+                <tr class="text-center">
+                    <td>0286315</td>
+                    <td><a href="{{ route('employee') }}">Mark Anthony H. Catubig</a></td>
+                    <td>mcatubig27@gmail.com</td>
+                    <td>Office Clerk</td>
+                    <td>School Division Office- Marikina</td>
+                </tr>
+                <tr class="text-center">
+                    <td>0286315</td>
+                    <td><a href="{{ route('employee') }}">John Lester Legaspi</a></td>
+                    <td>Legaspi@gmail.com</td>
+                    <td>Office Clerk</td>
+                    <td>School Division Office- Marikina</td>
+                </tr>
+                <tr class="text-center">
+                    <td>0286315</td>
+                    <td><a href="{{ route('employee') }}">John Ervin Rola</a></td>
+                    <td>rola@gmail.com</td>
+                    <td>Office Clerk</td>
+                    <td>School Division Office- Marikina</td>
+                </tr>
+                <tr class="text-center">
+                    <td>0286315</td>
+                    <td><a href="{{ route('employee') }}">Rinrin Caballero</a></td>
+                    <td>rinrin@gmail.com</td>
+                    <td>Office Clerk</td>
+                    <td>School Division Office- Marikina</td>
+                </tr>
+                <tr class="text-center">
+                    <td>0286315</td>
+                    <td><a href="{{ route('employee') }}">Charles Darwin Janoras</a></td>
+                    <td>charles@gmail.com</td>
+                    <td>Office Clerk</td>
+                    <td>School Division Office- Marikina</td>
+                </tr>
+                <tr class="text-center">
+                    <td>0286315</td>
+                    <td><a href="{{ route('employee') }}">Randell Ocmer</a></td>
+                    <td>ocmer@gmail.com</td>
+                    <td>Office Clerk</td>
+                    <td>School Division Office- Marikina</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
     <p id="noResults" style="display:none;">No results found.</p>
 
     <!--Bootstrap core JavaScript-->
@@ -98,7 +100,5 @@
     <!-- Javascript plugins-->
     <script src="{{ asset('javascript/searchemployee.js') }}" defer></script>
 
-</body>
-
+    </body>
 </html>
-
