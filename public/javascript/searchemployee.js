62bb81f527c6994d4acea2cdd99d5d558659f8ae
Randoq02 <randelclifford02@gmail.com>
2023-04-28 15:24:45 +0800

    test

--- public/javascript/searchemployee.js
+++ public/javascript/searchemployee.js
@@ -1,101 +1,81 @@
-function changeLabel(event) {
+// Set up event listener for search criteria dropdown
+document.querySelector('.dropdown-menu').addEventListener('click', function(event) {
     var target = event.target;
-    var newLabel = target.textContent.trim();
-    var dropdownButton = target.closest('.dropdown-menu').previousElementSibling;
-    dropdownButton.textContent = newLabel;
-
-    // Get table body and "No results found" message element
-    var tableBody = document.getElementById("tableBody");
-
-    function showTableHeading() {
-        var tableHeading = document.getElementById("tableHeading");
-        tableHeading.style.display = 'table-header-group';
+    if (target.matches('.dropdown-item')) {
+        var searchBy = target.getAttribute('data-field');
+        var newLabel = target.textContent.trim();
+        document.querySelector('.dropdown-toggle').textContent = newLabel;
+        filterTable(searchBy);
     }
+});
 
-    // Show table headings and table body when page loads
-    document.querySelector('#Employeetbl').style.display = 'table';
-
-    // Call showTableHeading to show table heading
-    showTableHeading();
-
-    // Hide table data initially
-    tableBody.style.display = 'none';
-
-    // Get table header element
-    var tableHeader = document.getElementById("Employeetbl");
-
-    function showTableHeader() {
-        tableHeader.style.display = 'table';
+// Set up event listener for search input
+document.querySelector('.search').addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+        filterTable();
     }
+});
 
-    // Call showTableHeader to show table header
-    showTableHeader();
+// Function to fetch table data from the server
+function getTableData(callback) {
+    var url = new URL('path/to/table/data');
+    url.searchParams.delete('_token');
+    url.searchParams.delete('searchby');
+    url.searchParams.delete('query');
+    url.searchParams.delete('result');
 
-    function filterTable() {
-        // Get input values
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            var data = JSON.parse(xhr.responseText);
+            callback(data);
+        }
+    };
+    xhr.send();
+}
+
+// Function to filter the table based on search criteria
+function filterTable(searchBy) {
+    getTableData(function(data) {
         var searchInput = document.querySelector('.search').value.toLowerCase();
-        var searchBy = document.querySelector('#searchby').value;
-
-        // Get table rows
-        var rows = document.querySelectorAll('#tableBody tr');
-
-        // Loop through all rows and hide those that do not match the search query
-        var noResults = true;
-        for (var i = 0; i < rows.length; i++) {
-            var cells = rows[i].querySelectorAll('td');
-            var shouldHide = true;
-            for (var j = 0; j < cells.length; j++) {
-                var cellText = cells[j].textContent.toLowerCase();
-                if (cells[j].getAttribute('data-field') === searchBy && cellText.indexOf(searchInput) !== -1) {
-                    shouldHide = false;
-                    noResults = false;
-                    break;
-                }
-            }
-            if (shouldHide) {
-                rows[i].style.display = 'none';
-            } else {
-                rows[i].style.display = '';
-            }
-        }
-
-        // Show/hide table and "No results found" message based on search query
-        if (searchInput === '') {
-            tableBody.style.display = 'none';
-            noResultsMsg.style.display = "none";
-        } else if (noResults) {
-            tableBody.innerHTML = "";
-            document.querySelector('.table').style.display = 'table';
-            noResultsMsg.style.display = "block";
-        } else {
-            tableBody.style.display = 'table-row-group';
-            noResultsMsg.style.display = "none";
-        }
-    }
-
-    // Add event listener to search input
-    document.querySelector('.search').addEventListener('keyup', function (event) {
-        // Check if Enter key was pressed
-        if (event.keyCode === 13) {
-            filterTable();
-        }
+        var rows = data.filter(function(row) {
+            return row[searchBy].toLowerCase().indexOf(searchInput) !== -1;
+        });
+        updateTable(rows);
     });
+}
 
-    // Add event listener to search criteria dropdown
-    var dropdownItems = document.querySelectorAll('.dropdown-item');
-    for (var i = 0; i < dropdownItems.length; i++) {
-        dropdownItems[i].addEventListener('click', function (event) {
-            // Set search criteria value
-            var searchBy = this.getAttribute('data-field');
-            document.querySelector('#searchby').value = searchBy;
+// Function to update the table with new rows
+function updateTable(rows) {
+    var tableBody = document.getElementById('tableBody');
+    var noResultsMsg = document.getElementById('noResultsMsg');
 
-            // Change dropdown button label
-            var dropdownButton = document.querySelector('.dropdown-toggle');
-            var newLabel = this.textContent;
-            dropdownButton.textContent = newLabel;
+    // Clear existing rows
+    tableBody.innerHTML = '';
 
-            // Call filter function
-            filterTable();
+    if (rows.length === 0) {
+        // Show "No results found" message
+        noResultsMsg.style.display = 'block';
+        tableBody.style.display = 'none';
+    } else {
+        // Hide "No results found" message
+        noResultsMsg.style.display = 'none';
+        tableBody.style.display = 'table-row-group';
+
+        // Add new rows to the table
+        rows.forEach(function(row) {
+            var tr = document.createElement('tr');
+            Object.keys(row).forEach(function(key) {
+                var td = document.createElement('td');
+                td.textContent = row[key];
+                td.setAttribute('data-field', key);
+                tr.appendChild(td);
+            });
+            tableBody.appendChild(tr);
         });
     }
 }
+
+// Initial call to update table with all rows
+filterTable();
